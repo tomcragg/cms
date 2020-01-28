ed1880a41bd2c94c8d58cbfd1ea3bf85bfa06dcc
Tom <tom.cragg1@gmail.com>
2020-01-28 21:56:27 +0100

    added 2 posts to DB changed index.php to show posts dynamically

--- includes/db.php
+++ includes/db.php
@@ -12,8 +12,8 @@ foreach($db as $key => $value){
 
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
-if($connection) {
-    echo "DB are connected";
-}
+// if($connection) {
+//     echo "DB are connected";
+// }
 
 ?>
